4f61509b1fde0a7ae131fe5dc0ec9c4fee75dce0
VioreliaMotofelea <viorelia.motofelea123@gmail.com>
2025-05-30 13:32:46 +0300

    Fix: install @fvilers/disable-react-devtools for production build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
+
+//const cors = require('cors');
+app.use(cors({ origin: 'https://mppanime-frontend.onrender.com' }));
+
+
 // app.use('/api/animes', animeRoutes); // Removed to avoid route conflict
 
 // Initialize TypeORM
