ed99881779b42a68d8d7aa71ad104216d80d0ef7
VioreliaMotofelea <viorelia.motofelea123@gmail.com>
2025-06-06 11:57:50 +0300

    api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Anime, AnimeListResponse } from '../types/anime';
 
-const API_URL = 'https://mppanime-backend.onrender.com';
+const API_URL = 'https://mppanime-backend.onrender.com/api';
 
 export const animeApi = {
   getAllAnimes: async (page = 1, limit = 10): Promise<AnimeListResponse> => {
