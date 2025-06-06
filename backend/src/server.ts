ea874ece11517741ebff61b48a46a4bd611f4bbd
VioreliaMotofelea <viorelia.motofelea123@gmail.com>
2025-06-06 12:15:45 +0300

    express.join

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-//app.use(express.json());
+app.use(express.json());
 
 //const cors = require('cors');
 app.use(cors({ origin: 'https://mppanime-frontend.onrender.com' }));
